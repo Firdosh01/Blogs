4cd6f1b584ac9eb79b927d4cd0d637216b6d8f81
Firdosh01 <firdosh7392@gmail.com>
2023-04-10 06:56:18 -0500

    little styling

--- src/Pagination.js
+++ src/Pagination.js
@@ -6,12 +6,13 @@ function Pagination() {
   const { page, handlePageChange, totalPages } = useContext(AppContext);
 
   return (
-    <div >
-      <div>
-        <div>
+    <div className='fixed bottom-0 flex items-center justify-center w-full bg-white border-2'>
+      <div  className='flex justify-between w-11/12 max-w-[670px] py-2'>
+        <div className='flex gap-x-2'>
 
           {page > 1 &&
             (<button
+              className='px-4 py-1 border-2 rounded-md'
               onClick={() => handlePageChange(page - 1)}>
               Previous
             </button>)
@@ -19,6 +20,7 @@ function Pagination() {
 
           {page < totalPages &&
             (<button
+              className='px-4 py-1 border-2 rounded-md'
               onClick={() => handlePageChange(page + 1)}>
               Next
             </button>)
@@ -27,7 +29,7 @@ function Pagination() {
         </div>
 
 
-        <p >
+        <p  className='text-sm font-bold'>
           Page {page} of {totalPages}
         </p>
       </div>
