9b5a352ed192492680e4b4a94c495e1cde1b1d76
Firdosh01 <firdosh7392@gmail.com>
2023-07-04 01:05:31 -0500

    update BlogPage

--- src/Components/Blogs.js
+++ src/Components/Blogs.js
@@ -1,35 +1,22 @@
 import React, { useContext } from 'react'
 import Spinner from './Spinner'
 import { AppContext } from './context/AppContext'
+import BlogDetails from './BlogDetails'
 
 function Blogs() {
   const {posts, loading} = useContext(AppContext)
 
   return (
-    <div className='w-11/12 max-w-[670px] h-screen py-8 flex flex-col gap-y-7  mt-[270px]  mb-[300px]   justify-center items-center'>
+    <div className='flex flex-col my-4 gap-y-7'>
       {
         loading ? (<Spinner />) :  
         (
           posts.length === 0 ? 
-          (<div>
-            <p>No Post Found</p>
+          (<div className='min-h-[80vh] w-full flex justify-center items-center'>
+            <p className='text-3xl font-bold text-center'>No Post Found</p>
           </div>) :
           (posts.map ( (post) => (
-            <div>
-                    <p className="text-lg font-bold "> {post.title}</p>
-
-                    <p className='text-sm mt-[4px]'>
-                      By <span>{post.author}</span> On <span>{post.category}</span>
-                    </p>
-                    <p className='text-sm mt-[4px]'>Posted On {post.date} </p>
-                    <p className='text-md mt-[14px]'>{post.content} </p>
-
-                    <div className='flex gap-x-3'>
-                      {post.tags.map( (tag, index) => {
-                        return <span key={index} className="text-blue-700 underline font-bold text-xs mt-[5px]"> {`#${tag}`}</span>
-                      })}
-                    </div>
-            </div>
+            <BlogDetails key={post.id} post={post} />
           )))
         )
       }
--- src/Components/Pages/BlogPage.js
+++ src/Components/Pages/BlogPage.js
@@ -1,9 +1,11 @@
 import React from 'react'
-import {useState} from 'react'
+import {useState, useEffect} from 'react'
 import { useLocation, useNavigation } from 'react-router-dom';
 import { AppContext } from '../context/AppContext';
 import { useContext } from 'react';
-
+import Header from '../Header';
+import BlogDetails from '../BlogDetails';
+import { baseUrl } from '../../baseUrl';
 
 function BlogPage() {
 
@@ -12,14 +14,72 @@ function BlogPage() {
   const location = useLocation();
   const navigation = useNavigation();
   const {setLoading, loading} = useContext(AppContext)
+  const blogId = location.pathname.split("/").at(-1)
 
   async function fetchRelatedBlogs() {
     setLoading(true)
+    let url = `${baseUrl}?blogId=${blogId}`;
+    try{
+      const res = await fetch(url)
+      const data = await res.json()
+
+      setBlog(data.blog);
+      setRelatedBlogs(data.relatedBlogs)
+    }
+    catch(error) {
+      console.log("Error in Blog ID")
+      setBlog(null)
+      setRelatedBlogs([])
+    }
+    setLoading(false)
   }
+
+  // useEffect(() => {
+  //   if(blogId)
+  //   {
+  //     fetchRelatedBlogs()
+  //   }
+  // },[location.pathname])
 
   return (
     <div>
-      
+      <Header />
+      <div>
+        <button
+        onClick={() => navigation(-1)}
+        >
+          Back
+        </button>
+      </div>
+      {
+        loading ? 
+        (
+          <div>
+            <p>Loading...</p>
+          </div>
+        ) :
+        blog ?
+        (
+          <div>
+            <BlogDetails post={blog} />
+            <h2>Related Blogs</h2>
+            {
+              relatedblogs.map((post) => {
+                <div key={post.id}>
+                  <BlogDetails post={post} />
+                </div>
+              })
+            }
+          </div>
+        ) :
+        (
+          <div>
+            <p>No Blog Found</p>
+          </div>
+        )
+
+        
+      }
     </div>
   )
 }
